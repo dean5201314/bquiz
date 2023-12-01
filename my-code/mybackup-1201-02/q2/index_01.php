e80e4a6fbedcd717c4a6edddfdfdfb23d20a2f7e
dean5201314 <smile90624@gmail.com>
2023-12-01 12:00:59 +0800

    bquiz no.2 + del

--- my-code/mybackup-1201-02/q2/index_01.php
+++ my-code/mybackup-1201-02/q2/index_01.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>問卷調查</title>
     <link rel="stylesheet" href="../css/bootstrap.css">
 </head>
-
 <body>
     <header class="p-5">
         <h1 class="text-center">問卷調查</h1>
